45cf6b387d1f0cfc0c8bc84341b6d4ba1be070e6
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2024-08-27 20:11:51 -0500

    Added functionality to the share button. It toggles hidden the share box and adds active styles.

---
+++ script.js
@@ -0,0 +1,7 @@
+const shareButton = document.querySelector("#share-btn");
+const shareBox = document.querySelector("#share-box");
+
+shareButton.addEventListener("click", () => {
+  shareBox.classList.toggle("hidden");
+  shareButton.classList.toggle("active");
+});
