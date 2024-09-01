9c63650b51f9744baa872dcc4d4431593105635f
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2024-08-31 20:37:18 -0500

    Added a blur to the button to lose foucs on mobile.

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ const shareBox = document.querySelector("#share-box");
 shareButton.addEventListener("click", () => {
   shareBox.classList.toggle("hidden");
   shareButton.classList.toggle("active");
+
+  shareButton.blur();
 });
